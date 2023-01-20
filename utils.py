a15ab8203c7b2a052d9a2cd45ea147691deeb0ea
Husky21st <husky21.mn@gmail.com>
2023-01-20 12:54:58 +0900

    change utils

--- utils.py
+++ utils.py
@@ -42,19 +42,9 @@ def get_iou(target_label_data, b):
 	iou_list = list()
 	for target_label_info in target_label_data:
 		a = target_label_info['roi']
-		# aは1つの矩形を表すshape=(4,)のnumpy配列
-		# array([xmin, ymin, xmax, ymax])
-		# bは任意のN個の矩形を表すshape=(N, 4)のnumpy配列
-		# 2次元目の4は、array([xmin, ymin, xmax, ymax])
-
-		# 矩形aの面積a_areaを計算
 		a_area = (a[2] - a[0] + 1) * (a[3] - a[1] + 1)
-		# bに含まれる矩形のそれぞれの面積b_areaを計算
-		# shape=(N,)のnumpy配列。Nは矩形の数
 		b_area = (b[:, 2] - b[:, 0] + 1) * (b[:, 3] - b[:, 1] + 1)
 
-		# aとbの矩形の共通部分(intersection)の面積を計算するために、
-		# N個のbについて、aとの共通部分のxmin, ymin, xmax, ymaxを一気に計算
 		abx_mn = np.maximum(a[0], b[:, 0])  # xmin
 		aby_mn = np.maximum(a[1], b[:, 1])  # ymin
 		abx_mx = np.minimum(a[2], b[:, 2])  # xmax
